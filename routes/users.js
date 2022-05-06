26cd005cc0a7a960dea8b34dbef123b5117ac34e
DhruvaVetsa12ABC <95221871+DhruvaVetsa12ABC@users.noreply.github.com>
2022-05-06 18:27:03 +0530

    Update on users.js

--- routes/users.js
+++ routes/users.js
@@ -3,15 +3,11 @@ mongoose.connect('mongodb://localhost:27017/interactive_periodic_table');
 
 let schema = mongoose.Schema(
   {
-    user_name_d : {
+    element : {
       type : "String",
       required : true
     },
-    game_name_d : {
-      type : "String",
-      required : true
-    },
-    review_d : {
+    note : {
       type : "String",
       required : true
     }
